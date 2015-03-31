af6ba9104d997b100937713e0693e196461a3264
danswick <dan.swick@gmail.com>
2015-03-31 18:32:57 -0500

    create leaflet boilerplate! :fire: :egg:
    simple template for creating leaflet maps

---
+++ Leaflet-boilerplate/js/script.js
@@ -0,0 +1,54 @@
+//basemap tiles (mapbox sat)
+var mapTiles = L.tileLayer('https://{s}.tiles.mapbox.com/v4/danswick.lff6ij2d/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiZGFuc3dpY2siLCJhIjoieUZiWmwtVSJ9.0cPQywdbPVmvHiHJ6NwdXA', {
+      attribution: "<a href='https://www.mapbox.com/about/maps/' target='_blank'>&copy; Mapbox &copy; OpenStreetMap</a> <a class='mapbox-improve-map' href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a>",
+      maxZoom: 17,
+});
+
+
+
+// ======= SWAP OUT THE FILENAME BELOW FOR YOUR FILE ======= //
+ var yourFilePath = "js/LM.json";
+
+// ========================================================= //
+
+// ==== OR CONSTRUCT ONE HERE AND SWAP THE VARIABLE NAME === //
+var yourGeoJson = [{
+  "type": "Feature",
+  "properties": {
+    "name": "Delta West",
+    "floor": "12"
+  },
+  "geometry": {
+    "type": "Point",
+    "coordinates": [-87.626867, 41.886493]
+  }
+}, {
+  "type": "Feature",
+  "properties": {
+    "name": "Delta East",
+    "floor": "B"
+  },
+  "geometry": {
+    "type": "Point",
+    "coordinates": [-84.560646, 42.726780]
+  }
+}];
+
+
+
+var geojson = L.geoJson(yourGeoJson);
+
+// ========================================================= //
+
+var map = L.map('map').fitBounds(geojson.getBounds());
+
+mapTiles.addTo(map);
+geojson.addTo(map);
+
+$.getJSON(yourFilePath, function(data){
+  // check for var with file path
+  if (yourFilePath != "undefined") {
+    var passedJson = L.geoJson(data);
+    passedJson.addTo(map);
+  }
+});
