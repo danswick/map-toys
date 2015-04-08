9d456b643fdca9bf5ad0b51ce72b07defdb9de3a
danswick <dan.swick@gmail.com>
2015-04-08 13:00:34 -0500

    mapbox boilerplate init! :fire: :egg:

---
+++ Mapbox-boilerplate/js/script.js
@@ -0,0 +1,3 @@
+L.mapbox.accessToken = "pk.eyJ1IjoiZGFuc3dpY2siLCJhIjoieUZiWmwtVSJ9.0cPQywdbPVmvHiHJ6NwdXA";
+var map = L.mapbox.map('map', 'mapbox.light')
+                  .setView([41.886493, -87.626867], 12);
