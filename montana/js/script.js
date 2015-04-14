3f7d90409177b25f6c5481b64e92befe2b7beab9
danswick <dan.swick@gmail.com>
2015-04-14 11:17:57 -0500

    pull geojson trail data from gist
    editable in geojson.io

--- montana/js/script.js
+++ montana/js/script.js
@@ -55,7 +55,7 @@ var map = L.mapbox.map('map', 'mapbox.outdoors')
 var pointLayer = L.mapbox.featureLayer().addTo(map);
 
 var trailLayer = L.mapbox.featureLayer()
-						 .loadURL('data/glacier_trails.geojson')
+						 .loadURL('https://gist.githubusercontent.com/danswick/70549ec10da7f81b6120/raw/glacier_trails.geojson')
 						 .addTo(map);
 
 // Add custom popups to each point
