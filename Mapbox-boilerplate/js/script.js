ad0bcc6ceb7966170081df2568ca6fb9fe107395
danswick <dan.swick@gmail.com>
2016-07-27 17:37:26 -0700

    add gl-version-test toy

--- Mapbox-boilerplate/js/script.js
+++ Mapbox-boilerplate/js/script.js
@@ -1,3 +1,7 @@
-L.mapbox.accessToken = "pk.eyJ1IjoiZGFuc3dpY2siLCJhIjoieUZiWmwtVSJ9.0cPQywdbPVmvHiHJ6NwdXA";
-var map = L.mapbox.map('map', 'mapbox.light')
-                  .setView([41.886493, -87.626867], 12);
+L.mapbox.accessToken = "pk.eyJ1IjoibGxvZyIsImEiOiI2TjRCZ1cwIn0.RDJVGBMizNkZpSTzYV7kOg";
+var map = L.mapbox.map('map','llog.h1113608', {
+    attributionControl: false,
+    center: [27.9674, -90.6889, 8],
+    minZoom: 6,
+    maxZoom: 10
+  });
